c2135c0f41a6fe82c71ca358397582d893a963a2
Freemason-12 <maksatlego@gmail.com>
2024-07-06 23:01:28 +0500

    feat: changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/freemason-12-REACT2024Q3/',
+	base: '/rsschool-react/',
 });
